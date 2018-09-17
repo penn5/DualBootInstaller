b8ba8e5dbf33c3c6b7f39c5fd9c5d49b8e261a2d
Hackintosh5 <penn.mackintosh@gmail.com>
2018-09-17 14:51:42 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+#Copyright 2018 Penn Mackintosh
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+print('This script expects to find a file "rom.zip" in the cwd')
+print('This file requires the following modules from pypi')
+print('brotli')
+import brotli
+print('zipfile')
+import zipfile
+
+
+
+#uncompress zip
+with zipfile.ZipFile('rom.zip', 'r') as zip:
+    zip.extractall('rom')
+if os.path.isfile('rom/system.new.data.br')
+    data = brotli.decompress(open('rom/system.new.dat.br', 'rb'))
+    with open('rom/system.new.dat', 'wb') as f:
+        f.write(data)
+
+print('decompressed brotli')
+
+import sdat2img
+sdat2img.main('rom/system.transfer.list', 'rom/system.new.dat', './system.img')
+
+
+print('sdat2img complete')
+
+input('Reboot the device into TWRP and connect USB, then press enter')
+
+from adb import adb_commands as adb
+
+device = adb.AdbCommands()
+device.ConnectDevice()
+device.Push('./system.img', '/sdcard/system.img')
+print('DO NOT DISCONNECT OR REBOOT PHONE')
+
+print(device.Shell('rm -rf /mnt/{};mkdir /mnt/{};mount -o loop -t auto /sdcard/system.img /mnt/{};rm -rf /system/{};mkdir /system/{};cp -af /mnt/{} /system'))
+
+print('if the above was succesful or blank, it worked!')
